f9300f8751c9b2ac8245d0c4f96c0cb199b862a9
John <johnlouisstephenson@gmail.com>
2025-07-02 10:42:41 +0100

    fix: remove obsolete aria-expanded from select

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,9 @@ export default function App() {
       <form>
         <h2>Harvest Details</h2>
         <label htmlFor="fruit">Fruit</label>
-        <select id="fruit" aria-expanded="false">
-          <option>Apples</option>
-          <option>Grapes</option>
+        <select id="fruit">
+          <option value="apples">Apples</option>
+          <option value="grapes">Grapes</option>
         </select>
         <label htmlFor="date">Harvest Date</label>
         <input id="date" type="date" />
