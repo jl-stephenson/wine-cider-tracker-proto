9d622ffa0cce263e701fa82acf9b3155d2c5fcbf
John <johnlouisstephenson@gmail.com>
2025-07-02 09:50:28 +0100

    test: failing test for app render

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,6 +5,16 @@ import App from "./App";
 describe("App", () => {
   it("renders App component", () => {
     render(<App />);
-    expect(screen.getByRole("heading", {name: /Harvest Details/i})).toBeInTheDocument();
+    expect(
+      screen.getByRole("heading", { level: 2, name: /Harvest Details/i }),
+    ).toBeInTheDocument();
+    expect(screen.getByRole("combobox")).toBeInTheDocument();
+    expect(
+      screen.getByRole("textbox", { name: /Harvest Date/i }),
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole("textbox", { name: /Weight/i }),
+    ).toBeInTheDocument();
+    expect(screen.getByRole("textbox", { name: /Notes/i })).toBeInTheDocument();
   });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,12 @@ export default function App() {
   return (
     <main>
       <form>
-      <h2>Harvest Details</h2>
+        <h2>Harvest Details</h2>
+        <label htmlFor="fruit"></label>
+        <select id="fruit" aria-expanded="false">
+          <option>Apples</option>
+          <option>Grapes</option>
+        </select>
       </form>
     </main>
   );
