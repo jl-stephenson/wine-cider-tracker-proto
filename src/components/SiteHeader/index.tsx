67d0cf39546c5f5432257c0a93ba4f240099fad5
John <johnlouisstephenson@gmail.com>
2025-07-09 12:58:47 +0100

    refactor: moved svg to components/icons

--- src/components/SiteHeader/index.tsx
+++ src/components/SiteHeader/index.tsx
@@ -1,3 +1,6 @@
+import { CloseIcon } from "../icons/CloseIcon";
+import { HamburgerIcon } from "../icons/HamburgerIcon";
+
 type SiteHeaderProps = {
   isSidebarOpen: boolean;
   toggleSidebar: () => void;
@@ -13,48 +16,7 @@ export function SiteHeader({ isSidebarOpen, toggleSidebar }: SiteHeaderProps) {
         aria-controls="sidebar"
         aria-expanded={isSidebarOpen}
       >
-        {isSidebarOpen ? (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            aria-hidden="true"
-            focusable="false"
-          >
-            <line
-              x1="4"
-              y1="4"
-              x2="20"
-              y2="20"
-              stroke="currentColor"
-              strokeWidth="2"
-              stroke-linecap="round"
-            />
-            <line
-              x1="4"
-              y1="20"
-              x2="20"
-              y2="4"
-              stroke="currentColor"
-              strokeWidth="2"
-              stroke-linecap="round"
-            />
-          </svg>
-        ) : (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            aria-hidden="true"
-            focusable="false"
-          >
-            <rect y="4" width="24" height="2" rx="1" fill="currentColor" />
-            <rect y="11" width="24" height="2" rx="1" fill="currentColor" />
-            <rect y="18" width="24" height="2" rx="1" fill="currentColor" />
-          </svg>
-        )}
+        {isSidebarOpen ? <CloseIcon /> : <HamburgerIcon />}
       </button>
       <p className="site-header__breadcrumb">Home {">"} Harvests</p>
       <div>
