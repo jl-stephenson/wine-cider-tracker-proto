24efa8ae5d0981da079c838514fe3f675f31f550
John <johnlouisstephenson@gmail.com>
2025-07-14 11:37:10 +0100

    chore: rename popover-button.css to menu-button.css

--- src/components/ActionsMenuButton/index.tsx
+++ src/components/ActionsMenuButton/index.tsx
@@ -1,23 +1,32 @@
-import { ChevronUpIcon } from "../icons/ChevronUpIcon";
+import { ChevronDownIcon } from "../icons/ChevronDownIcon";
 
-export function SplitButton() {
+export function ActionsMenuButton() {
   return (
-    <div className="split-button">
-      <button type="button" className="primary-action">
-        Process
+    <div className="menu-button">
+      <button
+        type="button"
+        id="menu-button1"
+        aria-haspopup="true"
+        aria-expanded="false"
+        aria-controls="menu1"
+      >
+        Actions
+        <ChevronDownIcon />
       </button>
-      <div className="action-menu-wrapper">
-      <details className="action-menu">
-        <summary>
-            <ChevronUpIcon />
-          <span className="visually-hidden">Other actions</span>
-        </summary>
-        <div className="menu-content">
-          <button type="button">Process now</button>
-          <button type="button">Set reminder</button>
-        </div>
-      </details>
-      </div>
+      <ul
+        id="menu1"
+        role="menu"
+        tabIndex={-1}
+        aria-labelledby="menu-button1"
+        aria-activedescendant="mi1"
+      >
+        <li id="mi1" role="menuitem">
+          Process
+        </li>
+        <li id="mi2" role="menuitem">
+          Set process date
+        </li>
+      </ul>
     </div>
   );
 }
