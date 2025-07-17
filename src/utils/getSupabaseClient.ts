2d77f64a314c67369e92511c9c4badba1d2316ad
John <johnlouisstephenson@gmail.com>
2025-07-17 15:31:36 +0100

    fix(SBclient): add validation for environment variables

--- src/utils/getSupabaseClient.ts
+++ src/utils/getSupabaseClient.ts
@@ -1,10 +1,14 @@
 import { createClient, SupabaseClient } from "@supabase/supabase-js";
 
 export function getSupabaseClient() {
-  const client: SupabaseClient = createClient(
-    import.meta.env.VITE_SUPABASE_URL,
-    import.meta.env.VITE_SUPABASE_API_KEY,
-  );
+  const url = import.meta.env.VITE_SUPABASE_URL;
+  const apiKey = import.meta.env.VITE_SUPABASE_API_KEY;
+
+  if (!url || !apiKey) {
+    throw new Error("Missing required Supabase environment variables");
+  }
+
+  const client: SupabaseClient = createClient(url, apiKey);
 
   return client;
 }
